411b996211560e596a407bdb8cb48438569d4f45
mebi36 <chukwudumebinwobi@gmail.com>
2022-06-02 07:32:10 +0100

    moved the test fixtures to the centralized fixtures.py file

--- course/tests.py
+++ course/tests.py
@@ -3,7 +3,7 @@ from rest_framework.test import APIRequestFactory
 
 from db.models import Department, Faculty, Semester, Course
 from course.views import CourseDetail, CourseList
-
+from tams_server.tests import fixtures
 
 class CourseListTestCase(TestCase):
     def setUp(self):
@@ -15,19 +15,9 @@ class CourseListTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
     
     def test_course_creation(self):
-        faculty = Faculty.objects.create(name="Engineering")
-        department = Department.objects.create(name="ECE", faculty=faculty)
-        request = self.factory.post(
-            'courses/', 
-            {
-                "code": "ECE 371",
-                "title": "System Programming",
-                "level_of_study":1,
-                "department":department.pk,
-                "unit_load":3,
-                "semester": Semester.FIRST
-            }
-        )
+        faculty = Faculty.objects.create(**fixtures.faculty_fixture)
+        department = Department.objects.create(**fixtures.department_fixture)
+        request = self.factory.post('courses/', fixtures.course_api_fixture)
         response = CourseList.as_view()(request)
         self.assertEqual(response.status_code, 201)
 
@@ -35,17 +25,10 @@ class CourseListTestCase(TestCase):
 class CourseDetailTestCase(TestCase):
     def setUp(self):
         self.factory = APIRequestFactory()
-        self.faculty = Faculty.objects.create(name="Engineering")
-        self.department = Department.objects.create(name="ECE", faculty=self.faculty)
-        course_details = {
-            "code": "ECE 371",
-            "title": "System Programming",
-            "level_of_study":1,
-            "department":self.department,
-            "unit_load":3,
-            "semester": Semester.FIRST
-        }
-        self.course = Course.objects.create(**course_details)
+        self.faculty = Faculty.objects.create(**fixtures.faculty_fixture)
+        self.department = Department.objects.create(**fixtures.department_fixture)
+
+        self.course = Course.objects.create(**fixtures.course_fixture)
     
     def test_course_detail(self):
         request = self.factory.get('courses/')
@@ -53,30 +36,15 @@ class CourseDetailTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
     
     def test_course_edit(self):
-        changed_course_title = {
-            "id": self.course.pk,
-            "code": "ECE 371",
-            "title": "General Programming",
-            "level_of_study":1,
-            "department":self.department.pk,
-            "unit_load":3,
-            "semester": Semester.FIRST,
-            "elective":False,
-            "is_active":True
-        }
+        changed_course_title = fixtures.course_api_fixture
+        changed_course_title["title"] = "General Programming"
         request = self.factory.put('courses/{}/'.format(self.course.pk), changed_course_title)
         response = CourseDetail.as_view()(request, pk=self.course.pk)
         self.assertEqual(response.status_code, 200)
     
-    def test_course_edit_result(self):       
-        changed_course_title = {
-            "code": "ECE 371",
-            "title": "General Programming",
-            "level_of_study":1,
-            "department":self.department.pk,
-            "unit_load":3,
-            "semester": Semester.FIRST
-        }
+    def test_course_edit_result(self):
+        changed_course_title = fixtures.course_api_fixture
+        changed_course_title["title"] = "General Programming"
         request = self.factory.put('courses/', changed_course_title)
         response = CourseDetail.as_view()(request, pk=self.course.pk)
         self.assertEqual(response.data["title"], changed_course_title["title"])
