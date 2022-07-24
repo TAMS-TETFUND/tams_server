40f4c25081808a26900c58960ec64caf673947e9
mebi36 <chukwudumebinwobi@gmail.com>
2022-07-24 10:29:15 +0100

    removed unnecessary comments and added args to the HttpForbidden error class call

--- attendance/views.py
+++ attendance/views.py
@@ -16,84 +16,14 @@ from db.models import (
 from attendance.serializers import AttendanceSessionSerializer
 
 
-# def download_attendance(request, pk):
-#     template = "download.html"
-#     attendance_session = AttendanceSession.objects.get(id=pk)
-
-#     if (
-#         attendance_session.initiator is None
-#         or attendance_session.initiator.username != request.user.username
-#     ):
-#         message = {
-#             "details": "Permission denied. You did not initiate this attendance session."
-#         }
-#         return render(request, template, message)
-
-#     qs = (
-#         AttendanceRecord.objects.filter(
-#             Q(
-#                 Q(attendance_session=attendance_session)
-#                 & Q(attendance_session__initiator=request.user)
-#             )
-#         )
-#         .prefetch_related("student")
-#         .values(
-#             "student__first_name",
-#             "student__last_name",
-#             "student__reg_number",
-#             "student__department__name",
-#             "logged_by",
-#         )
-#     )
-
-#     if not qs.exists():
-#         message = {"details": "No attendance records were found for the event"}
-#         return render(request, template, message)
-
-#     response = HttpResponse(
-#         content_type="text/csv",
-#         headers={
-#             f"Content-Disposition": "attachment; filename="
-#             f"{attendance_session.course.code} "
-#             f'Attendance {datetime.strftime(attendance_session.start_time, "%d-%m-%Y")}.csv'
-#         },
-#     )
-
-#     field_names = ["S/N", "Name", "Reg. Number", "Department", "Sign In"]
-#     attendance_writer = csv.DictWriter(response, fieldnames=field_names)
-#     attendance_writer.writerow(
-#         {
-#             "S/N": f'{attendance_session.course.code} Attendance {datetime.strftime(attendance_session.start_time, "%d-%m-%Y")}'
-#         }
-#     )
-#     attendance_writer.writeheader()
-#     for idx, row in enumerate(qs, 1):
-#         attendance_writer.writerow(
-#             {
-#                 "S/N": idx,
-#                 "Name": f'{row["student__last_name"].capitalize()} {row["student__first_name"].capitalize()}',
-#                 "Reg. Number": row["student__reg_number"],
-#                 "Department": row["student__department__name"],
-#                 "Sign In": f'{datetime.strftime(row["logged_by"], "%H:%M")}',
-#             }
-#         )
-
-#     return response
-
-# @login_required
 def download_attendance(request, pk):
-    # template = "download.html"
     attendance_session = AttendanceSession.objects.get(id=pk)
 
     if (
         attendance_session.initiator is None
         or attendance_session.initiator.username != request.user.username
     ):
-        # message = {
-        #     "details": "Permission denied. You did not initiate this attendance session."
-        # }
-        # return render(request, template, message)
-        return HttpResponseForbidden()
+        return HttpResponseForbidden("403: Permission Denied")
 
     qs = (
         AttendanceRecord.objects.filter(
@@ -109,7 +39,7 @@ def download_attendance(request, pk):
             "student__reg_number",
             "student__department",
             "student__department__name",
-            "student__department__faculty"
+            "student__department__faculty",
             "logged_by",
         )
     )
@@ -120,9 +50,7 @@ def download_attendance(request, pk):
             qs = qs.filter(student__department=request.POST["department"])
 
     if not qs.exists():
-        # message = {"details": "No attendance records were found for the event"}
-        # return render(request, template, message)
-        return HttpResponseServerError()
+        return HttpResponseServerError("Error: No records found")
 
     response = HttpResponse(
         content_type="text/csv",
