176147a853b9fc849ea50559fd2089425272dbe0
mebi36 <chukwudumebinwobi@gmail.com>
2022-08-24 17:44:53 +0100

    changed the courselist and coursedetail views to generic views

--- course/serializers.py
+++ course/serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
 
 from db.models import Course, Department
-
+from department.serializers import DepartmentSerializer
 
 class CourseSerializer(serializers.ModelSerializer):
+    department = DepartmentSerializer(many=False, read_only=True)
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+
+class CourseUpdateSerializer(serializers.ModelSerializer):
     department = serializers.PrimaryKeyRelatedField(
         queryset=Department.objects.all(), many=False
     )
-
     class Meta:
         model = Course
         fields = "__all__"
--- course/views.py
+++ course/views.py
@@ -1,52 +1,30 @@
-from django.http import Http404
 
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from db.models import Course
-from course.serializers import CourseSerializer
+from course.serializers import CourseSerializer, CourseUpdateSerializer
 
 
-class CourseDetail(APIView):
+class CourseDetail(RetrieveUpdateDestroyAPIView):
     """Retrieve, update or delete a course instance."""
+    queryset = Course.objects.all()
 
-    def get_object(self, pk):
-        try:
-            return Course.objects.get(pk=pk)
-        except Course.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        course = self.get_object(pk)
-        serializer = CourseSerializer(course)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        course = self.get_object(pk)
-        serializer = CourseSerializer(course, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        course = self.get_object(pk)
-        course.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def get_serializer_class(self):
+        if self.request.method in ('PUT', 'PATCH'):
+            return CourseUpdateSerializer
+        return CourseSerializer
 
 
-class CourseList(APIView):
+class CourseList(ListCreateAPIView):
     """List all courses, or create a new course."""
+    def get_queryset(self):
+        queryset = Course.objects.all()
+        department_name = self.request.query_params.get('department')
+        if department_name is not None:
+            queryset = queryset.filter(department__name=department_name)
+        return queryset
 
-    def get(self, request, format=None):
-        courses = Course.objects.all()
-        serializer = CourseSerializer(courses, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = CourseSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CourseUpdateSerializer
+        return CourseSerializer
