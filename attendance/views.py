0929a472dd6d150996029539bec9ed320da9b0c4
mebi36 <chukwudumebinwobi@gmail.com>
2022-09-10 17:26:11 +0100

    added api_view decorator to fix request.user issues

--- attendance/views.py
+++ attendance/views.py
@@ -13,6 +13,7 @@ from django.db.models import Q, F
 from django.contrib.auth.decorators import login_required
 
 from rest_framework import generics
+from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
@@ -34,21 +35,20 @@ from db.models import (
     Student,
 )
 
-# @login_required
-# figure out how to use users/me
+
+@api_view(["GET"])
+@login_required
 def download_attendance(request, pk):
     attendance_session = AttendanceSession.objects.get(id=pk)
-    print("the request username is %s" % request.user.username)
 
-    # if (
-    #         attendance_session.initiator is None
-    #         or attendance_session.initiator.username != request.user.username
-    # ):
-    #     return HttpResponseForbidden(
-    #         "403: Permission Denied %s here %s" % (request.user.username, request.__dict__)
-    #     )
+    if (
+        attendance_session.initiator is None
+        or attendance_session.initiator.username != request.user.username
+    ):
+        return HttpResponseForbidden("403: Permission Denied")
 
-    qs = (AttendanceRecord.objects.filter(attendance_session=attendance_session)
+    qs = (
+        AttendanceRecord.objects.filter(attendance_session=attendance_session)
         .prefetch_related("student")
         .values(
             "student__first_name",
@@ -73,8 +73,8 @@ def download_attendance(request, pk):
         content_type="text/csv",
         headers={
             f"Content-Disposition": "attachment; filename="
-                                    f"{attendance_session.course.code} "
-                                    f'Attendance {datetime.strftime(attendance_session.start_time, "%d-%m-%Y")}.csv'
+            f"{attendance_session.course.code} "
+            f'Attendance {datetime.strftime(attendance_session.start_time, "%d-%m-%Y")}.csv'
         },
     )
 
@@ -99,57 +99,69 @@ def download_attendance(request, pk):
 
     return response
 
+
 def download_collated_attendance(request):
     """
     This view is for processing and returning a CSV file of collated attendance
     for a course the user making the request has initiated an attendance for.
-    
+
     get all attendance sessions for a course in the given session,
     get all the students that ever attended an event for the course,
     for each student check attedance for every attendance session list from step one
     """
-    course = request.query_params.get('course')
-    session = request.query_params.get('session')
+    course = request.query_params.get("course")
+    session = request.query_params.get("session")
     if course is None or session is None:
-        return HttpResponseBadRequest('Invalid request: specify course and session')
-    
-    
+        return HttpResponseBadRequest(
+            "Invalid request: specify course and session"
+        )
+
     if not (course_obj := Course.objects.filter(pk=course)).exists():
-        return Http404('Course does not exist')
+        return Http404("Course does not exist")
     if not (session_obj := AcademicSession.objects.filter(pk=session)).exists():
-        return Http404('Academic Session does not exist')
-    
+        return Http404("Academic Session does not exist")
+
     course_obj = course_obj.first()
     session_obj = session_obj.first()
 
-    all_course_events = AttendanceSession.objects.filter(course=course_obj, session=session_obj).order_by('-start_time')
-    course_events_list = all_course_events.values_list('id', flat=True)
-    all_attendance_records = AttendanceRecord.objects.filter(attendance_session__in=course_events_list)
-    students_in_attendance = set(all_attendance_records.order_by('-student__last_name').values_list('student__reg_number', 'student__first_name', 'student_last_name', 'student__department__name', 'student__level_of_study'))
+    all_course_events = AttendanceSession.objects.filter(
+        course=course_obj, session=session_obj
+    ).order_by("-start_time")
+    course_events_list = all_course_events.values_list("id", flat=True)
+    all_attendance_records = AttendanceRecord.objects.filter(
+        attendance_session__in=course_events_list
+    )
+    students_in_attendance = set(
+        all_attendance_records.order_by("-student__last_name").values_list(
+            "student__reg_number",
+            "student__first_name",
+            "student_last_name",
+            "student__department__name",
+            "student__level_of_study",
+        )
+    )
     attendance_tally = {}
 
     for student in students_in_attendance:
         student_tally = []
         for event in course_events_list:
-            if AttendanceRecord.objects.filter(student_id=student[0], attendance_session=event).exists():
-                student_tally.append('PRESENT')
+            if AttendanceRecord.objects.filter(
+                student_id=student[0], attendance_session=event
+            ).exists():
+                student_tally.append("PRESENT")
             else:
-                student_tally.append('ABSENT')
+                student_tally.append("ABSENT")
         attendance_tally[student] = student_tally
 
-    
     response = HttpResponse(
         content_type="text/csv",
         headers={
             f"Content-Disposition": "attachment; filename="
-                                    f"{course_obj.code} {session_obj.session} Collated Attendance.csv"
-        }
+            f"{course_obj.code} {session_obj.session} Collated Attendance.csv"
+        },
     )
 
     # field_names = ["Reg_number", "Name"] + [session_date for session_date in ]
-
-    
-
 
 
 class AttendanceSessionPagination(PageNumberPagination):
@@ -169,23 +181,32 @@ class AttendanceSessionList(generics.ListCreateAPIView):
                 "attendance_session", flat=True
             )
         )
+        print("rest_framework knows the user is: ", self.request.user.username)
         return AttendanceSession.objects.filter(
             initiator__isnull=False,
             id__in=sessions_with_records,
             initiator_id=self.request.user.username,
-        ).order_by('-start_time')
+        ).order_by("-start_time")
 
 
 class AttendanceSessionByCourseList(generics.ListAPIView):
     """Lists all attendance sessions for all courses the user making request has
     ever initiated an attendance for.
     """
+
     serializer_class = AttendanceSessionSerializer
 
     def get_queryset(self):
-        sessions_with_records = set(AttendanceRecord.objects.values_list("attendance_session", flat=True))
+        sessions_with_records = set(
+            AttendanceRecord.objects.values_list(
+                "attendance_session", flat=True
+            )
+        )
         courses_by_sessions = list(
-            AttendanceSession.objects.filter(id__in=sessions_with_records, initiator_id=self.request.user.username)
+            AttendanceSession.objects.filter(
+                id__in=sessions_with_records,
+                initiator_id=self.request.user.username,
+            )
             .values("course", "session")
             .distinct()
         )
@@ -206,35 +227,53 @@ class AttendanceSessionByCourseList(generics.ListAPIView):
 
 class AttendanceSessionByCourseDetail(APIView):
     """Gives a breakdown of student attendance rates for a course."""
+
     def get(self, request):
         context = {}
-        course = request.query_params.get('course')
-        session = request.query_params.get('session')
+        course = request.query_params.get("course")
+        session = request.query_params.get("session")
 
-        course_data = CourseSerializer(Course.objects.get(pk=course), many=False)
-        context['course'] = course_data.data
+        course_data = CourseSerializer(
+            Course.objects.get(pk=course), many=False
+        )
+        context["course"] = course_data.data
 
-        session_data = AcademicSessionSerializer(AcademicSession.objects.get(pk=session), many=False)
-        context['session'] = session_data.data
+        session_data = AcademicSessionSerializer(
+            AcademicSession.objects.get(pk=session), many=False
+        )
+        context["session"] = session_data.data
 
+        all_records = AttendanceRecord.objects.filter(
+            attendance_session__course__id=course,
+            attendance_session__session__id=session,
+        )
 
-        all_records = AttendanceRecord.objects.filter(attendance_session__course__id=course, attendance_session__session__id=session)
-    
-        all_sessions = AttendanceSessionSerializer(AttendanceSession.objects.filter(course_id=course, session_id=session).order_by('start_time'), many=True)
-        context['all_sessions'] = all_sessions.data
+        all_sessions = AttendanceSessionSerializer(
+            AttendanceSession.objects.filter(
+                course_id=course, session_id=session
+            ).order_by("start_time"),
+            many=True,
+        )
+        context["all_sessions"] = all_sessions.data
 
-        students = set(all_records.values_list('student', flat=True))
+        students = set(all_records.values_list("student", flat=True))
         attendance_details = []
         for student in students:
-            attendance_details.append({
-                "student": StudentSerializer(Student.objects.get(pk=student)).data,
-                "valid_check_ins": AttendanceRecordSerializer(all_records.filter(student_id=student, is_valid=True), many=True).data
-            })
+            attendance_details.append(
+                {
+                    "student": StudentSerializer(
+                        Student.objects.get(pk=student)
+                    ).data,
+                    "valid_check_ins": AttendanceRecordSerializer(
+                        all_records.filter(student_id=student, is_valid=True),
+                        many=True,
+                    ).data,
+                }
+            )
 
-        context['student_attendance'] = attendance_details
+        context["student_attendance"] = attendance_details
 
         return Response(context)
-
 
 
 class AttendanceList(APIView):
@@ -254,13 +293,15 @@ class AttendanceList(APIView):
         for record_data in request.data:
             serializer = AttendanceRecordSerializer(data=record_data)
             if not serializer.is_valid():
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    serializer.errors, status=status.HTTP_400_BAD_REQUEST
+                )
             try:
                 serializer.save()
             except IntegrityError:
                 continue
 
-        response = {'details': 'successfully synced'}
+        response = {"details": "successfully synced"}
         return Response(response, status=status.HTTP_201_CREATED)
 
 
@@ -318,5 +359,3 @@ class StudentAttendanceList(APIView):
             ).data
             student_attendance_report.append(item)
         return Response(student_attendance_report)
-
-
