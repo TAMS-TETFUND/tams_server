8336a056e896ac9928f077f65421d93954b6669c
mebi36 <chukwudumebinwobi@gmail.com>
2022-10-24 01:52:54 +0100

    readded authentication methods that was erroneously removed

--- tams_server/settings.py
+++ tams_server/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = cred_dict["credentials"]["SECRET_KEY"]
 DEBUG = True
 # DEBUG = False
 
-ALLOWED_HOSTS = ["192.168.43.243", "localhost", "127.0.0.1", "192.168.0.128"]
+ALLOWED_HOSTS = ["192.168.43.243", "localhost", "127.0.0.1", "192.168.0.128", "192.168.6.243"]
 
 # Application definition
 
