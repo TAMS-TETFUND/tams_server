3c72533a97afae0e47f9fb19d70421f2b62c45fc
mebi36 <chukwudumebinwobi@gmail.com>
2022-10-24 01:52:20 +0100

    reformatted with black

--- staff/views.py
+++ staff/views.py
@@ -26,10 +26,10 @@ class StaffDetail(APIView):
     def put(self, request, pk, format=None):
         staff = self.get_object(pk.upper())
         serializer = StaffSerializer(staff, data=request.data)
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-
         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
